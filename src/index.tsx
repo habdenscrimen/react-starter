c611dbda35e2990871e097bac5405ddcbca39b70
Viacheslav Yefremov <habdenscrimen@gmail.com>
2020-04-01 17:39:42 +0300

    feat: setup project

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React, { FunctionComponent } from 'react'
+import ReactDOM from 'react-dom'
+
+const App: FunctionComponent = () => <main>App</main>
+
+ReactDOM.render(<App />, document.querySelector('#root'))
